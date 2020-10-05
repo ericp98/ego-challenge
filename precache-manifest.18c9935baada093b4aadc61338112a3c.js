b1e72a4cb00ff6e82cf09e15dbe25461300f7f17
Eric <Eric>
2020-10-05 11:18:41 -0300

    Updates

---
+++ precache-manifest.18c9935baada093b4aadc61338112a3c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117adb5775831a8d5a8359be843ed9f9",
+    "url": "/ego-challenge/index.html"
+  },
+  {
+    "revision": "fa7651ee2ba31e94709c",
+    "url": "/ego-challenge/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "77b420adc59cd8796d0b",
+    "url": "/ego-challenge/static/css/main.2dd05b8a.chunk.css"
+  },
+  {
+    "revision": "fa7651ee2ba31e94709c",
+    "url": "/ego-challenge/static/js/2.9295285d.chunk.js"
+  },
+  {
+    "revision": "d3031346469a8a2ef527f1ad705ea3b7",
+    "url": "/ego-challenge/static/js/2.9295285d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77b420adc59cd8796d0b",
+    "url": "/ego-challenge/static/js/main.9138e8be.chunk.js"
+  },
+  {
+    "revision": "09931042304c693cb852",
+    "url": "/ego-challenge/static/js/runtime-main.cab4dcc7.js"
+  },
+  {
+    "revision": "84fb968e7f05df0acf20b8acf4e47903",
+    "url": "/ego-challenge/static/media/logo.84fb968e.svg"
+  },
+  {
+    "revision": "42351577795ea968382ac795a9671e8c",
+    "url": "/ego-challenge/static/media/notFound.42351577.png"
+  }
+]);
